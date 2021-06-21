0647a3983cc42f3019e84be7616c5442403eab7a
Dave Mateer <davemateer@gmail.com>
2021-06-21 10:47:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using AzureSDKConsole;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Network.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+
+
+// source https://github.com/Azure-Samples/compute-dotnet-create-virtual-machines-from-generalized-image-or-specialized-vhd
+
+
+var userName = "dave";
+//var password = SdkContext.RandomResourceName("Pa5$", 15);
+var password = "lAADDDA22!!";
+
+List<string> ApacheInstallScriptUris = new() { "https://raw.githubusercontent.com/Azure/azure-libraries-for-net/master/Samples/Asset/install_apache.sh" };
+var apacheInstallCommand = "bash install_apache.sh";
+
+// Authenticate
+//var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+var credentials = SdkContext.AzureCredentialsFactory.FromFile(@"c:\temp\my.azureauth");
+
+var sw = Stopwatch.StartNew();
+var azure = Azure
+    .Configure()
+    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+    .Authenticate(credentials)
+    .WithDefaultSubscription();
+
+Console.WriteLine("Selected subscription: " + azure.SubscriptionId);
+
+//string rgName = SdkContext.RandomResourceName("AzureSDKTEST", 10);
+var unixTimestamp = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+string rgName = $"azure{unixTimestamp}";
+
+//string linuxVmName1 = SdkContext.RandomResourceName("VM1", 10);
+string linuxVmName1 = "vm";
+
+//string publicIpDnsLabel = SdkContext.RandomResourceName("azuresdktest", 10);
+string publicIpDnsLabel = rgName;
+
+try
+{
+    // resource group
+    Console.WriteLine($"creating rg {rgName}");
+    var resourceGroup = azure.ResourceGroups
+        .Define(rgName)
+        .WithRegion(Region.EuropeWest)
+        .Create();
+
+    // network - vnet
+    Console.WriteLine("creating network - vnet");
+    var network = azure.Networks.Define("mynetwork")
+        .WithRegion(Region.EuropeWest)
+        //.WithNewResourceGroup()
+        .WithExistingResourceGroup(resourceGroup)
+        .WithAddressSpace("10.0.0.0/28")
+        .WithSubnet("Front-end", "10.0.0.0/29")
+        //.WithSubnet("subnet2", "10.0.0.8/29")
+        .Create();
+
+    // nsg
+    Console.WriteLine("creating nsg and 2 rules");
+    var frontEndNSG = azure.NetworkSecurityGroups.Define("nsg")
+        .WithRegion(Region.EuropeWest)
+        //.WithNewResourceGroup(rgName)
+        .WithExistingResourceGroup(resourceGroup)
+        .DefineRule("ALLOW-SSH")
+            .AllowInbound()
+            .FromAnyAddress()
+            .FromAnyPort()
+            .ToAnyAddress()
+            .ToPort(22)
+            .WithProtocol(SecurityRuleProtocol.Tcp)
+            .WithPriority(100)
+            .WithDescription("Allow SSH")
+            .Attach()
+        .DefineRule("ALLOW-HTTP")
+            .AllowInbound()
+            .FromAnyAddress()
+            .FromAnyPort()
+            .ToAnyAddress()
+            .ToPort(80)
+            .WithProtocol(SecurityRuleProtocol.Tcp)
+            .WithPriority(101)
+            .WithDescription("Allow HTTP")
+            .Attach()
+        .Create();
+
+    // nic
+    Console.WriteLine("creating nic");
+    var networkInterface1 = azure.NetworkInterfaces.Define("nic")
+        .WithRegion(Region.EuropeWest)
+        //.WithExistingResourceGroup(rgName)
+        .WithExistingResourceGroup(resourceGroup)
+        .WithExistingPrimaryNetwork(network)
+        .WithSubnet("Front-end")
+        .WithPrimaryPrivateIPAddressDynamic()
+        .WithNewPrimaryPublicIPAddress(publicIpDnsLabel)
+        .WithIPForwarding()
+        .WithExistingNetworkSecurityGroup(frontEndNSG)
+        .Create();
+
+
+    // vm
+    Console.WriteLine("Creating a Linux VM");
+    var linuxVM = azure.VirtualMachines.Define(linuxVmName1)
+            .WithRegion(Region.EuropeWest)
+            //.WithNewResourceGroup(rgName)
+            .WithExistingResourceGroup(resourceGroup)
+            //.WithNewPrimaryNetwork("10.0.0.0/28")
+            //.WithPrimaryPrivateIPAddressDynamic()
+            //.WithNewPrimaryPublicIPAddress(publicIpDnsLabel)
+            .WithExistingPrimaryNetworkInterface(networkInterface1)
+            // Nice strongly typed image names - but old
+            //.WithPopularLinuxImage(KnownLinuxVirtualMachineImage.UbuntuServer16_04_Lts)
+            // az vm image list --publisher Canonical --sku 20_04-lts-gen2 --output table --all
+            //.WithLatestLinuxImage("Canonical", "0001-com-ubuntu-server-focal", "20_04-lts-gen2")
+            //.WithLatestLinuxImage("Canonical", "0001-com-ubuntu-server-focal", "20_04-lts")
+            .WithLatestLinuxImage("Canonical", "0001-com-ubuntu-server-focal", "20_04-lts")
+            .WithRootUsername(userName)
+            .WithRootPassword(password)
+            // look into this need pem format
+            //.WithSsh()
+            // **look into this **
+            //.WithUnmanagedDisks()
+            // Nice strongly typed machine size - but need a crib sheet for costs
+            // https://azure.microsoft.com/en-gb/pricing/details/virtual-machines/linux/
+            //.WithSize(VirtualMachineSizeTypes.Parse("Standard_D2a_v4"))
+            // £5 per month
+            .WithSize(VirtualMachineSizeTypes.StandardB1ms)
+            
+
+            .DefineNewExtension("CustomScriptForLinux")
+                .WithPublisher("Microsoft.OSTCExtensions")
+                .WithType("CustomScriptForLinux")
+                .WithVersion("1.4")
+                .WithMinorVersionAutoUpgrade()
+                // pulling a bash script from a Uri
+                .WithPublicSetting("fileUris", ApacheInstallScriptUris)
+                // sending the bash script
+                .WithPublicSetting("commandToExecute", apacheInstallCommand)
+                .Attach()
+            .Create();
+
+    Console.WriteLine("Created a Linux VM: " + linuxVM.Id);
+    Utilities.PrintVirtualMachine(linuxVM);
+    Console.WriteLine();
+
+    var conn = $"ssh -o StrictHostKeyChecking=no dave@{rgName}.westeurope.cloudapp.azure.com";
+    Console.WriteLine(conn);
+    Console.WriteLine(password);
+    
+    //// De-provision the virtual machine
+    //Utilities.DeprovisionAgentInLinuxVM(linuxVM.GetPrimaryPublicIPAddress().Fqdn, 22, UserName, Password);
+
+    ////=============================================================
+    //// Deallocate the virtual machine
+    //Console.WriteLine("Deallocate VM: " + linuxVM.Id);
+
+    //linuxVM.Deallocate();
+
+    //Console.WriteLine("Deallocated VM: " + linuxVM.Id + "; state = " + linuxVM.PowerState);
+
+    ////=============================================================
+    //// Generalize the virtual machine
+    //Console.WriteLine("Generalize VM: " + linuxVM.Id);
+
+    //linuxVM.Generalize();
+
+    //Console.WriteLine("Generalized VM: " + linuxVM.Id);
+
+    ////=============================================================
+    //// Capture the virtual machine to get a 'Generalized image' with Apache
+    //Console.WriteLine("Capturing VM: " + linuxVM.Id);
+
+    //var capturedResultJson = linuxVM.Capture("capturedvhds", "img", true);
+
+    //Console.WriteLine("Captured VM: " + linuxVM.Id);
+
+    ////=============================================================
+    //// Create a Linux VM using captured image (Generalized image)
+    //JObject o = JObject.Parse(capturedResultJson);
+    //JToken resourceToken = o.SelectToken("$.resources[?(@.properties.storageProfile.osDisk.image.uri != null)]");
+    //if (resourceToken == null)
+    //{
+    //    throw new Exception("Could not locate image uri under expected section in the capture result -" + capturedResultJson);
+    //}
+    //string capturedImageUri = (string)(resourceToken["properties"]["storageProfile"]["osDisk"]["image"]["uri"]);
+
+    //Console.WriteLine("Creating a Linux VM using captured image - " + capturedImageUri);
+
+    //var linuxVM2 = azure.VirtualMachines.Define(linuxVmName2)
+    //        .WithRegion(Region.USWest)
+    //        .WithExistingResourceGroup(rgName)
+    //        .WithNewPrimaryNetwork("10.0.0.0/28")
+    //        .WithPrimaryPrivateIPAddressDynamic()
+    //        .WithoutPrimaryPublicIPAddress()
+    //        .WithStoredLinuxImage(capturedImageUri) // Note: A Generalized Image can also be an uploaded VHD prepared from an on-premise generalized VM.
+    //        .WithRootUsername(UserName)
+    //        .WithRootPassword(Password)
+    //        .WithSize(VirtualMachineSizeTypes.Parse("Standard_D2a_v4"))
+    //        .Create();
+
+    //Utilities.PrintVirtualMachine(linuxVM2);
+
+    //var specializedVhd = linuxVM2.OSUnmanagedDiskVhdUri;
+    ////=============================================================
+    //// Deleting the virtual machine
+    //Console.WriteLine("Deleting VM: " + linuxVM2.Id);
+
+    //azure.VirtualMachines.DeleteById(linuxVM2.Id); // VM required to be deleted to be able to attach it's
+    //                                               // OS Disk VHD to another VM (Deallocate is not sufficient)
+
+    //Console.WriteLine("Deleted VM");
+
+    ////=============================================================
+    //// Create a Linux VM using 'specialized VHD' of previous VM
+
+    //Console.WriteLine("Creating a new Linux VM by attaching OS Disk vhd - "
+    //        + specializedVhd
+    //        + " of deleted VM");
+
+    //var linuxVM3 = azure.VirtualMachines.Define(linuxVmName3)
+    //        .WithRegion(Region.USWest)
+    //        .WithExistingResourceGroup(rgName)
+    //        .WithNewPrimaryNetwork("10.0.0.0/28")
+    //        .WithPrimaryPrivateIPAddressDynamic()
+    //        .WithoutPrimaryPublicIPAddress()
+    //        .WithSpecializedOSUnmanagedDisk(specializedVhd, OperatingSystemTypes.Linux) // New user credentials cannot be specified
+    //        .WithSize(VirtualMachineSizeTypes.Parse("Standard_D2a_v4"))         // when attaching a specialized VHD
+    //        .Create();
+
+    //Utilities.PrintVirtualMachine(linuxVM3);
+}
+finally
+{
+    try
+    {
+        Console.WriteLine("Press any key to delete the rg");
+        Console.ReadLine();
+        Console.WriteLine("Deleting rg");
+        //Console.WriteLine("Deleting Resource Group: " + rgName);
+        azure.ResourceGroups.DeleteByName(rgName);
+        //Console.WriteLine("Deleted Resource Group: " + rgName);
+    }
+    catch
+    {
+        Console.WriteLine("Did not create any resources in Azure. No clean up is necessary");
+    }
+}
+
+Console.WriteLine($"done in {sw.ElapsedMilliseconds}ms");
+
---
+++ Utilities.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Compute.Fluent;
+
+namespace AzureSDKConsole
+{
+    public static class Utilities
+    {
+        public static void PrintVirtualMachine(IVirtualMachine virtualMachine)
+        {
+            var storageProfile = new StringBuilder().Append("\n\tStorageProfile: ");
+            if (virtualMachine.StorageProfile.ImageReference != null)
+            {
+                storageProfile.Append("\n\t\tImageReference:");
+                storageProfile.Append("\n\t\t\tPublisher: ").Append(virtualMachine.StorageProfile.ImageReference.Publisher);
+                storageProfile.Append("\n\t\t\tOffer: ").Append(virtualMachine.StorageProfile.ImageReference.Offer);
+                storageProfile.Append("\n\t\t\tSKU: ").Append(virtualMachine.StorageProfile.ImageReference.Sku);
+                storageProfile.Append("\n\t\t\tVersion: ").Append(virtualMachine.StorageProfile.ImageReference.Version);
+            }
+
+            if (virtualMachine.StorageProfile.OsDisk != null)
+            {
+                storageProfile.Append("\n\t\tOSDisk:");
+                storageProfile.Append("\n\t\t\tOSType: ").Append(virtualMachine.StorageProfile.OsDisk.OsType);
+                storageProfile.Append("\n\t\t\tName: ").Append(virtualMachine.StorageProfile.OsDisk.Name);
+                storageProfile.Append("\n\t\t\tCaching: ").Append(virtualMachine.StorageProfile.OsDisk.Caching);
+                storageProfile.Append("\n\t\t\tCreateOption: ").Append(virtualMachine.StorageProfile.OsDisk.CreateOption);
+                storageProfile.Append("\n\t\t\tDiskSizeGB: ").Append(virtualMachine.StorageProfile.OsDisk.DiskSizeGB);
+                if (virtualMachine.StorageProfile.OsDisk.Image != null)
+                {
+                    storageProfile.Append("\n\t\t\tImage Uri: ").Append(virtualMachine.StorageProfile.OsDisk.Image.Uri);
+                }
+                if (virtualMachine.StorageProfile.OsDisk.Vhd != null)
+                {
+                    storageProfile.Append("\n\t\t\tVhd Uri: ").Append(virtualMachine.StorageProfile.OsDisk.Vhd.Uri);
+                }
+                if (virtualMachine.StorageProfile.OsDisk.EncryptionSettings != null)
+                {
+                    storageProfile.Append("\n\t\t\tEncryptionSettings: ");
+                    storageProfile.Append("\n\t\t\t\tEnabled: ").Append(virtualMachine.StorageProfile.OsDisk.EncryptionSettings.Enabled);
+                    storageProfile.Append("\n\t\t\t\tDiskEncryptionKey Uri: ").Append(virtualMachine
+                            .StorageProfile
+                            .OsDisk
+                            .EncryptionSettings
+                            .DiskEncryptionKey.SecretUrl);
+                    storageProfile.Append("\n\t\t\t\tKeyEncryptionKey Uri: ").Append(virtualMachine
+                            .StorageProfile
+                            .OsDisk
+                            .EncryptionSettings
+                            .KeyEncryptionKey.KeyUrl);
+                }
+            }
+
+            if (virtualMachine.StorageProfile.DataDisks != null)
+            {
+                var i = 0;
+                foreach (var disk in virtualMachine.StorageProfile.DataDisks)
+                {
+                    storageProfile.Append("\n\t\tDataDisk: #").Append(i++);
+                    storageProfile.Append("\n\t\t\tName: ").Append(disk.Name);
+                    storageProfile.Append("\n\t\t\tCaching: ").Append(disk.Caching);
+                    storageProfile.Append("\n\t\t\tCreateOption: ").Append(disk.CreateOption);
+                    storageProfile.Append("\n\t\t\tDiskSizeGB: ").Append(disk.DiskSizeGB);
+                    storageProfile.Append("\n\t\t\tLun: ").Append(disk.Lun);
+                    if (virtualMachine.IsManagedDiskEnabled)
+                    {
+                        if (disk.ManagedDisk != null)
+                        {
+                            storageProfile.Append("\n\t\t\tManaged Disk Id: ").Append(disk.ManagedDisk.Id);
+                        }
+                    }
+                    else
+                    {
+                        if (disk.Vhd.Uri != null)
+                        {
+                            storageProfile.Append("\n\t\t\tVhd Uri: ").Append(disk.Vhd.Uri);
+                        }
+                    }
+                    if (disk.Image != null)
+                    {
+                        storageProfile.Append("\n\t\t\tImage Uri: ").Append(disk.Image.Uri);
+                    }
+                }
+            }
+            StringBuilder osProfile;
+            if (virtualMachine.OSProfile != null)
+            {
+                osProfile = new StringBuilder().Append("\n\tOSProfile: ");
+
+                osProfile.Append("\n\t\tComputerName:").Append(virtualMachine.OSProfile.ComputerName);
+                if (virtualMachine.OSProfile.WindowsConfiguration != null)
+                {
+                    osProfile.Append("\n\t\t\tWindowsConfiguration: ");
+                    osProfile.Append("\n\t\t\t\tProvisionVMAgent: ")
+                            .Append(virtualMachine.OSProfile.WindowsConfiguration.ProvisionVMAgent);
+                    osProfile.Append("\n\t\t\t\tEnableAutomaticUpdates: ")
+                            .Append(virtualMachine.OSProfile.WindowsConfiguration.EnableAutomaticUpdates);
+                    osProfile.Append("\n\t\t\t\tTimeZone: ")
+                            .Append(virtualMachine.OSProfile.WindowsConfiguration.TimeZone);
+                }
+                if (virtualMachine.OSProfile.LinuxConfiguration != null)
+                {
+                    osProfile.Append("\n\t\t\tLinuxConfiguration: ");
+                    osProfile.Append("\n\t\t\t\tDisablePasswordAuthentication: ")
+                            .Append(virtualMachine.OSProfile.LinuxConfiguration.DisablePasswordAuthentication);
+                }
+            }
+            else
+            {
+                osProfile = new StringBuilder().Append("\n\tOSProfile: null");
+            }
+
+
+            var networkProfile = new StringBuilder().Append("\n\tNetworkProfile: ");
+            foreach (var networkInterfaceId in virtualMachine.NetworkInterfaceIds)
+            {
+                networkProfile.Append("\n\t\tId:").Append(networkInterfaceId);
+            }
+
+            var msi = new StringBuilder().Append("\n\tMSI: ");
+            msi.Append("\n\t\tMSI enabled:").Append(virtualMachine.IsManagedServiceIdentityEnabled);
+            msi.Append("\n\t\tMSI Active Directory Service Principal Id:").Append(virtualMachine.SystemAssignedManagedServiceIdentityPrincipalId);
+            msi.Append("\n\t\tMSI Active Directory Tenant Id:").Append(virtualMachine.SystemAssignedManagedServiceIdentityTenantId);
+
+            //Utilities.Log(new StringBuilder().Append("Virtual Machine: ").Append(virtualMachine.Id)
+            Console.Write(new StringBuilder().Append("Virtual Machine: ").Append(virtualMachine.Id)
+                    .Append("Name: ").Append(virtualMachine.Name)
+                    .Append("\n\tResource group: ").Append(virtualMachine.ResourceGroupName)
+                    .Append("\n\tRegion: ").Append(virtualMachine.Region)
+                    .Append("\n\tTags: ").Append(FormatDictionary(virtualMachine.Tags))
+                    .Append("\n\tHardwareProfile: ")
+                    .Append("\n\t\tSize: ").Append(virtualMachine.Size)
+                    .Append(storageProfile)
+                    .Append(osProfile)
+                    .Append(networkProfile)
+                    .Append(msi)
+                    .ToString());
+
+        }
+
+        private static string FormatDictionary(IReadOnlyDictionary<string, string> dictionary)
+        {
+            if (dictionary == null)
+            {
+                return string.Empty;
+            }
+
+            var outputString = new StringBuilder();
+
+            foreach (var entity in dictionary)
+            {
+                outputString.AppendLine($"{entity.Key}: {entity.Value}");
+            }
+
+            return outputString.ToString();
+        }
+
+    }
+}
